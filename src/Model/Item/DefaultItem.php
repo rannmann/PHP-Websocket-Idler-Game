bbe2d41e1185ee004683752bc74283c944d48269
Jake Forrester <jake@ranndesigns.com>
2016-09-23 16:56:15 -0700

    Added some vendoring handlers to DefaultItem... although this should probably be in a controller

--- src/Model/Item/DefaultItem.php
+++ src/Model/Item/DefaultItem.php
@@ -10,4 +10,21 @@ class DefaultItem {
     //public $color =
     public $origin = ItemAttribute::ORIGIN_PURCHASED;
     public $image = '/img/404.png';
+
+
+    public function canVendor() {
+        return in_array(ItemAttribute::TRAIT_VENDORABLE, $this->traits);
+    }
+
+    public function onVendor(\Idler\Model\Player &$player) {
+        if (!$this->canVendor()) {
+            throw new \Exception("This item cannot be sold to the vendor.");
+        }
+        if (empty($this->vendorData) || empty($this->vendorData['price'])) {
+            throw new \Exception("This item is missing a price");
+        }
+        // Todo: Does this user actually own this item?
+        $player->coins += $this->vendorData['price'];
+        // Todo: Remove this?  How should we handle selling multiple items?
+    }
 }
